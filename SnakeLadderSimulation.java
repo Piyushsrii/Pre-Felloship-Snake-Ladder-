dc4eec3408f3a6942f48a9196f9559c393c59046
piyushsrii <piyushnamo888@gmail.com>
2020-09-11 15:52:17 +0530

    [Piyush] Modified : Repeat the code untill player get wining position

--- SnakeLadderSimulation.java
+++ SnakeLadderSimulation.java
@@ -14,24 +14,46 @@
                System.out.println(""+randomNum);
                return rollDie;
          }
-          public static void casesInSankeLader()
-         {
-               int random1To6=rollDie;
-               Random random= new Random();
-               int randomNum=random.nextInt(3)+1;
-               System.out.println(randomNum);
-         if(randomNum==1)
-         {
-               System.out.println("You can't move you have done ome mistake");
-         }
-         else if(randomNum==2)
-         {
-               System.out.println("you stand on the snake, you will move back"+random1To6);
-         }
-         else
-         {
-               System.out.println("congrats you got ladder, you will move ahead by"+random1To6);
-         }
+                 public static void repeatSankeLader()
+                 {
+                 Random random = new Random();
+		 int Wining=0;
+		 while(Wining < 100)
+		 {
+			int random1To6=rollDie();
+			int RandomCheck=random.nextInt(3);
+			if(RandomCheck ==2 && Wining > random1To6)
+			{
+
+				System.out.println("you stand on the snake, you will move back by = " +random1To6);
+				Wining=Wining-random1To6;
+				System.out.println("you are in "+Wining);
+
+			}
+			else if(RandomCheck == 0)
+			{
+				System.out.println("No Play");
+				System.out.println("you are in "+Wining);
+			}
+			else
+			{
+				Wining=Wining+random1To6;
+				if(Wining > 100)
+				{
+					Wining=Wining-random1To6;
+				}
+				else {
+					System.out.println("congrats you have incremented by "+random1To6);
+					System.out.println("you are in "+Wining);
+				}
+			}
+			if(Wining == 100)
+			{
+				System.out.println("congrats you have reached till 100");
+                        }
+		}
+    }
+
          public static void main(String[] args)
          {
                startPosition();
