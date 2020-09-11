a9f017471fa626101fec6a176ad6efb111622dbf
piyushsrii <piyushnamo888@gmail.com>
2020-09-11 14:22:51 +0530

    [Piyush] Modified : Create Method to fixed the position

--- SnakeLadderSimulation.java
+++ SnakeLadderSimulation.java
@@ -1,1 +1,14 @@
 #### Welcome in Snake & Ladder Simulation #####
+
+    public class SnakeLadderSimulation
+    {
+
+         static void startPosition()
+         {
+         System.out.println("Player starting from position 0");
+         }
+         public static void main(String[] args)
+         {
+         startPosition();
+         }
+    }
