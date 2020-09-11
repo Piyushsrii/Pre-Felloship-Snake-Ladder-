c2de4b07df52e3927b97444f330a3154bf95d212
piyushsrii <piyushnamo888@gmail.com>
2020-09-11 14:46:18 +0530

    [Piyush] Modified : Given all the cases to player position for snake & ladder position by generating another random Variable

--- SnakeLadderSimulation.java
+++ SnakeLadderSimulation.java
@@ -5,17 +5,42 @@
 
          static void startPosition()
          {
-         System.out.println("Player starting from position 0");
+               System.out.println("Player starting from position 0");
          }
          public static void rollDie()
          {
-         Random random=new Random();
-         int randomNum=random.nextInt(6)+1;
-         System.out.println(""+randomNum);
+               Random random=new Random();
+               int randomNum=random.nextInt(6)+1;
+               System.out.println(""+randomNum);
+         }
+          public static void casesInSankeLader()
+         {
+               Random random= new Random();
+               int randomNum=random.nextInt(3)+1;
+               System.out.println(randomNum);
+         if(randomNum==1)
+         {
+               System.out.println("You can't move you have done ome mistake");
+         }
+         else if(randomNum==2)
+         {
+               System.out.println("you stand on the snake, you will move two step back");
+         }
+         else
+         {
+               System.out.println("congrats you got ladder, you will move two forward");
          }
          public static void main(String[] args)
          {
-         startPosition();
-         rollDie();
+               startPosition();
+               rollDie();
+               casesInSankeLader();
          }
+
+         public static void main(String[] args)
+         {
+               startPosition();
+               rollDie();
+         }
+
     }
