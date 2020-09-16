be7db0e688c13ebb060e846fab389a08b4ae1a41
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-16 11:40:47 +0530

    [Piyush] Modify : Remove unwanted spaces and give proper indentation

--- SnakeLadderSimulation.java
+++ SnakeLadderSimulation.java
@@ -1,68 +1,62 @@
 #### Welcome in Snake & Ladder Simulation #####
     import java.util.Random;
-    public class SnakeLadderSimulation
-    {
-
-         static void startPosition()
-         {
-               System.out.println("Player starting from position 0");
-         }
-         public static int rollDie()
-         {
-               Random random=new Random();
-               int randomNum=random.nextInt(6)+1;
-               System.out.println(""+randomNum);
-               return rollDie;
-         }
-                 public static void repeatSankeLader()
-                 {
-                 Random random = new Random();
-		 int Wining=0;
-		 while(Wining < 100)
-		 {
-			int random1To6=rollDie();
+public class SnakeLadder
+{
+	public static void StartFrom0()
+	{
+		System.out.println("Player Start from 0");
+	}
+	public static int GetRandomNo1To6()
+	{
+		Random random = new Random();
+		int RandonNo1To6 = random.nextInt(6)+1;
+		//System.out.println("random number is "+RandonNo1To6);
+		return RandonNo1To6;
+	}
+	public static void RepeteTillPlayerReachExact100()
+	{ 
+		Random random = new Random();
+		int Win=0;
+                //to repeat till 100
+		while(Win < 100)
+		{
+			int RandonNo1To6=GetRandomNo1To6();
 			int RandomCheck=random.nextInt(3);
-			if(RandomCheck ==2 && Wining > random1To6)
+			if(RandomCheck == 2 && Win > RandonNo1To6)
 			{
-
-				System.out.println("you stand on the snake, you will move back by = " +random1To6);
-				Wining=Wining-random1To6;
-				System.out.println("you are in "+Wining);
-
-                                 if(Wining<0)
-                                {
-                                Wining=position;
-                                System.out.println("Now your real posiion is 0"+Wining);
-                                }
-
+				System.out.println("oops you step on snake you will go "+RandonNo1To6+" step back");
+				Win=Win-RandonNo1To6;
+				System.out.println("you are in "+Win);
 			}
 			else if(RandomCheck == 0)
 			{
-				System.out.println("No Play");
-				System.out.println("you are in "+Wining);
+				System.out.println("you made a foult you scored 0 so you will be in same position");
+				System.out.println("you are in "+Win);
 			}
-			else
+			else 
 			{
-				Wining=Wining+random1To6;
-                                if(Wining > 100)
+				Win=Win+RandonNo1To6;
+                                //to stop player exact in 100
+				if(Win > 100)
 				{
-					Wining=Wining-random1To6;
+					Win=Win-RandonNo1To6;
 				}
-
-				System.out.println("congrats you have incremented by "+random1To6);
-		         	System.out.println("you are in "+Wining);
+				else 
+                                {
+					System.out.println("congrats you have incremented by "+RandonNo1To6);
+					System.out.println("you are in "+Win);
+				}
 			}
-			if(Wining == 100)
+			if(Win == 100)
 			{
 				System.out.println("congrats you have reached till 100");
-                        }
+			}
 		}
-    }
-
-         public static void main(String[] args)
-         {
-               startPosition();
-               casesInSankeLader();
-         }
-
-    }
+	}
+	public static void main(String[] args)
+	{
+		StartFrom0();
+		System.out.println("..............");
+		RepeteTillPlayerReachExact100();
+	}
+}
