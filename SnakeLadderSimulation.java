5665240b11456619cbb8cb9f6eb9f815b3f66355
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-16 11:34:57 +0530

    [Piyush] Modify : Remove all unwanted Spaces and give proper indentation

--- SnakeLadderSimulation.java
+++ SnakeLadderSimulation.java
@@ -1,42 +1,42 @@
 #### Welcome in Snake & Ladder Simulation #####
-    import java.util.Random;
-    public class SnakeLadderSimulation
-    {
+import java.util.Random;
+public class SnakeLadder
+{
+	public static void StartFrom0()
+	{
+		System.out.println("Player Start from 0");
+	}
+	public static int GetRandomNo1To6()
+	{
+		Random random = new Random();
+		int RandonNo1To6 = random.nextInt(6)+1;
+		System.out.println("random number is "+RandonNo1To6);
+		return RandonNo1To6;
+	}
 
-         static void startPosition()
-         {
-               System.out.println("Player starting from position 0");
-         }
-         public static int rollDie()
-         {
-               Random random=new Random();
-               int randomNum=random.nextInt(6)+1;
-               System.out.println(""+randomNum);
-               return rollDie;
-         }
-          public static void casesInSankeLader()
-         {
-               int random1To6=rollDie;
-               Random random= new Random();
-               int randomNum=random.nextInt(3)+1;
-               System.out.println(randomNum);
-         if(randomNum==1)
-         {
-               System.out.println("You can't move you have done ome mistake");
-         }
-         else if(randomNum==2)
-         {
-               System.out.println("you stand on the snake, you will move back"+random1To6);
-         }
-         else
-         {
-               System.out.println("congrats you got ladder, you will move ahead by"+random1To6);
-         }
-         public static void main(String[] args)
-         {
-               startPosition();
-               rollDie();
-               casesInSankeLader();
-         }
+         
+	public static void CheckSnake_Ladder_Noplay()
+	{ 
+         int RandonNo1To6=GetRandomNo1To6();
+         Random random = new Random();
+         int RandomCheck=random.nextInt(3);
+         	if(RandomCheck == 2)
+			{
+				System.out.println("oops you step on snake you will got 2 step back");
+			}
+			else if(RandomCheck == 0)
+			{
+				System.out.println("you made a foult so you will be in same position");
+			}
+			else
+				System.out.println("congrats you have incremented by "+RandonNo1To6);
 
-    }
+        }
+
+	public static void main(String[] args)
+	{
+		StartFrom0();
+		System.out.println(GetRandomNo1To6());
+		CheckSnake_Ladder_Noplay();
+	}
+ }
