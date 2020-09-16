dff14090b77a20b50ed58ff96aba14a549556459
Piyushsrii <67362417+Piyushsrii@users.noreply.github.com>
2020-09-16 11:43:20 +0530

    [Piyush] Modify : Convert variable into proper indentation and remove spaces

--- SnakeLadderSimulation.java
+++ SnakeLadderSimulation.java
@@ -1,73 +1,80 @@
 #### Welcome in Snake & Ladder Simulation #####
-    import java.util.Random;
-    public class SnakeLadderSimulation
-    {
-         static int position=0;
-         static void startPosition()
-         {
-               System.out.println("Player starting from position"+position);
-         }
-         public static int rollDie()
-         {
-               Random random=new Random();
-               int randomNum=random.nextInt(6)+1;
-               System.out.println(""+randomNum);
-               return rollDie;
-         }
-                 public static int numOfDiceRoll()
-                 {
-                 int countDiceRolling=0;
-                 Random random = new Random();
-		 int Wining=0;
-		 while(Wining < 100)
-		 {
-			int random1To6=rollDie();
-			int RandomCheck=random.nextInt(3);
-			if(RandomCheck ==2 && Wining > random1To6)
+   import java.util.Random;
+public class SnakeLadder
+{
+	public static void startFrom0()
+	{
+		System.out.println("You are Starting from 0");
+	}
+	public static int getRandomNo1To6()
+	{
+		Random random = new Random();
+		int randonNo1To6 = random.nextInt(6)+1;
+		//System.out.println("random number is "+RandonNo1To6);
+		return randonNo1To6;
+	}
+	public static int getNoOfTimesDicePlayed_Position()
+	{ 
+
+		Random random = new Random();
+		int noOfTimesDicePlayed=0;
+		int Win = 0;
+		while(Win < 100)
+		{
+			int randonNo1To6 = getRandomNo1To6();
+			int randomCheck = random.nextInt(3);
+			//if player position will go below 0 then player will start from 0
+
+			//when step on snake
+			if(randomCheck == 2 && Win > randonNo1To6)
 			{
-
-				System.out.println("you stand on the snake, you will move back by = " +random1To6);
-				Wining=Wining-random1To6;
-				System.out.println("you are in "+Wining);
-                                countDiceRolling++;
-                                if(Wining<0)
-                                {
-                                Wining=position;
-                                System.out.println("Now your real posiion is 0"+Wining);
-                                }
-
+				System.out.println("OOPS.. you step on snake you will go "+randonNo1To6+" step back");
+				Win = Win-randonNo1To6;
+				System.out.println("You are in position "+Win);
+				System.out.println();
+				noOfTimesDicePlayed++;
 			}
-			else if(RandomCheck == 0)
+			// no move 
+			else if(randomCheck == 0)
 			{
-				System.out.println("No Play");
-				System.out.println("you are in "+Wining);
-			        countDiceRolling++;
-                        }
-			else
+				System.out.println("You made a FOUL you scored 0 so you will be in same position");
+				System.out.println("You are in position "+Win);
+				System.out.println();
+				noOfTimesDicePlayed++;
+			}
+
+			else 
 			{
-				Wining=Wining+random1To6;
-                                if(Wining > 100)
+				Win = Win+randonNo1To6;
+				//if win exceed 100
+				if(Win > 100)
 				{
-					Wining=Wining-random1To6;
+					Win = Win-randonNo1To6;
+					noOfTimesDicePlayed++;
 				}
-				System.out.println("congrats you have incremented by "+random1To6);
-		         	System.out.println("you are in "+Wining);
-                                countDiceRolling++;
+				else {
+					System.out.println("You have scored "+randonNo1To6);
+					System.out.println("CONGRATS... you have incremented by "+randonNo1To6);
+					System.out.println("You are in position "+Win);
+					System.out.println();
+					noOfTimesDicePlayed++;
+				}
 			}
-			if(Wining == 100)
+			if(Win == 100)
 			{
-				System.out.println("congrats you have reached till 100");
-                        }
+				System.out.println("CONGRATS YOU HAVE WON THE GAME");
+				System.out.println();
+			}
 		}
-                   return countDiceRolling
-    }
+		return noOfTimesDicePlayed;
+	}
+	public static void main(String[] args)
+	{
+		startFrom0();
+		System.out.println("..............");
+		System.out.println("Total times dice played by you is "+getNoOfTimesDicePlayed_Position());
+		System.out.println();
+		System.out.println("Thankyou for playing game...");
+	}
+}
 
-         public static void main(String[] args)
-         {
-               startPosition();
-               System.out.println("<-------------->");
-               System.out.println("Total times dice played by you is"+numOfDiceRoll();
-               System.out.println("ThankYou For playing game");
-         }
-
-    }
